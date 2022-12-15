d1eac5cad9fa7a79298c78d70fdeafeb4b79cba3
Leticia Pinheiro <leticia.gomezpinheiro@gmail.com>
2022-12-14 22:07:40 -0300

    component button

---
+++ src/components/button.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+width: 326px;
+height: 46px;
+border: none;
+background: #A328D6;
+border-radius: 5px;
+color: #ffffff;
+font-family: 'Raleway';
+font-style: normal;
+font-weight: 700;
+font-size: 20px;
+line-height: 23px;`
+
+export { Button }
---
+++ src/components/title.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+const Title = styled.div`
+width: 326px;
+display: flex;
+align-items: center;
+justify-content: space-between;
+margin: 25px 0 0 24px;
+span{
+    color: #ffffff;
+    font-family: 'Raleway';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 26px;
+    line-height: 31px;
+}
+ion-icon{
+    cursor: pointer;
+    color: #ffffff;
+    font-size: 25px;
+    margin-right: 10px;
+}`
+
+export {Title}
