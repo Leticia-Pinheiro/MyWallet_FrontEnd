4625eca007bf0f0df7197bbe4c17991f1b8b6934
Leticia Pinheiro <leticia.gomezpinheiro@gmail.com>
2022-12-14 22:09:35 -0300

    component link

---
+++ src/components/link.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Link = styled.span `        
+font-family: 'Roboto', sans-serif;
+font-style: normal;
+font-weight: 400;
+font-size: 15px;        
+text-align: center;
+text-decoration-line: underline;
+color: #FFFFFF;`
+
+export { Link };
