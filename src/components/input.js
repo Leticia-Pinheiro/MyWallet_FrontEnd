e41c0631f88fde7ffe5c549a695af9ed958987c8
Leticia Pinheiro <leticia.gomezpinheiro@gmail.com>
2022-12-14 22:03:17 -0300

    component input

---
+++ src/components/input.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+const Input = styled.input `
+margin-bottom: 13px;
+box-sizing: border-box;    
+width: 326px;
+height: 58px;
+background: #FFFFFF;   
+border-radius: 5px;
+border: none;
+font-family: 'Raleway';
+font-style: normal;
+font-weight: 400;
+font-size: 20px;
+line-height: 23px; 
+color: #000000  
+padding-left: 15px;   
+::placeholder{        
+    color:#7E7E7E;        
+}`
+
+export {Input}
